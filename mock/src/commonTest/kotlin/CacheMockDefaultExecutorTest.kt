d78bb18cadcc9cb3e0d72753d7c0a982311fc5c7
andylamax <andylamax@programmer.net>
2023-04-22 11:32:10 +0300

    Ingored OSX during testing keep

--- mock/src/commonTest/kotlin/CacheMockDefaultExecutorTest.kt
+++ mock/src/commonTest/kotlin/CacheMockDefaultExecutorTest.kt
@@ -2,10 +2,12 @@ import keep.internal.AbstractCacheTest
 import kommander.expect
 import keep.CacheMock
 import keep.CacheMockConfig
+import kommander.IgnoreOSX
 import koncurrent.Executors
 
 import kotlin.test.Test
 
+@IgnoreOSX
 class CacheMockDefaultExecutorTest : AbstractCacheTest(CacheMock(config)) {
 
     companion object {
--- test/src/commonMain/kotlin/keep/internal/AbstractCacheTest.kt
+++ test/src/commonMain/kotlin/keep/internal/AbstractCacheTest.kt
@@ -1,6 +1,5 @@
 package keep.internal
 
-import kommander.IgnoreOSX
 import keep.Cache
 import keep.exceptions.CacheLoadException
 import keep.load
@@ -12,7 +11,6 @@ import koncurrent.later.test
 import kotlinx.serialization.Serializable
 import kotlin.test.Test
 
-@IgnoreOSX
 abstract class AbstractCacheTest(val cache: Cache) {
     @Test
     fun should_be_able_to_load_and_save_primitively_easily() = cache.save("int", 1).andThen {
